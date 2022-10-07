f00c56a075a4d28a39328288e295f24388c9d1c4
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-07 14:29:09 +0200

    implementazione della classe Conto

--- src/com/soprasteria/javaoopbank/conto/Conto.java
+++ src/com/soprasteria/javaoopbank/conto/Conto.java
@@ -1,5 +1,36 @@
 package com.soprasteria.javaoopbank.conto;
 
+import java.util.Random;
+
 public class Conto {
+	
+	private int numeroConto;
+	private String nomeProprietario;
+	private double saldo;
+	
+	private Random r;
+	
+	public Conto(String nomeProprietario) {
+		super();
+		this.numeroConto = r.nextInt(1000)+1;
+		this.nomeProprietario = nomeProprietario;
+		this.saldo = 0;
+	}
+
+	public String getNomeProprietario() {
+		return nomeProprietario;
+	}
+
+	public void setNomeProprietario(String nomeProprietario) {
+		this.nomeProprietario = nomeProprietario;
+	}
+
+	public int getNumeroConto() {
+		return numeroConto;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
 
 }
