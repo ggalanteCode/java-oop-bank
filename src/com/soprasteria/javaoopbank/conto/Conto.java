c1f31dacca72eb2476bd7514efacbf913962ee75
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-07 16:56:37 +0200

    il saldo è in euro

--- src/com/soprasteria/javaoopbank/conto/Conto.java
+++ src/com/soprasteria/javaoopbank/conto/Conto.java
@@ -51,7 +51,7 @@ public class Conto {
 	}
 	
 	private String contoFormattato() {
-		return String.format("Il saldo attuale sul conto è: %.2f", saldo);
+		return String.format("Il saldo attuale sul conto è: %.2f €", saldo);
 	}
 
 }
