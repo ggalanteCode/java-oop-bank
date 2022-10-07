69d9d65125a9b123c28aed0e77f8bc65b7991e77
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-07 16:07:37 +0200

    ora l'utente può effettuare le operazioni di versamento e prelievo

--- src/com/soprasteria/javaoopbank/Bank.java
+++ src/com/soprasteria/javaoopbank/Bank.java
@@ -9,6 +9,7 @@ public class Bank {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String utente;
+		int opzione, sommaDaVersarePrelevare;
 		
 		Scanner s = new Scanner(System.in);
 		
@@ -16,6 +17,38 @@ public class Bank {
 		utente = s.nextLine();
 		
 		Conto myConto = new Conto(utente);
+		
+		do {
+			stampaMenu();
+			opzione = s.nextInt();
+			s.nextLine();
+			switch(opzione) {
+			case 1:
+				System.out.print("Quanti soldi vuoi versare sul tuo conto?:");
+				sommaDaVersarePrelevare = s.nextInt();
+				myConto.versamento(sommaDaVersarePrelevare);
+				System.out.println(myConto.informazioniConto());
+				break;
+			case 2:
+				System.out.print("Quantio soldi vuoi prelevare dal tuo conto?:");
+				sommaDaVersarePrelevare = s.nextInt();
+				if(myConto.prelievo(sommaDaVersarePrelevare) == -1) {
+					System.err.println("ERRORE: NON HAI ABBASTANZA SOLDI SUL TUO CONTO!");
+				} else {
+					System.out.println(myConto.informazioniConto());
+				}
+				break;
+			case 3:
+				break;
+			}
+		} while(opzione != 3);
+	}
+	
+	private static void stampaMenu() {
+		System.out.println("Selezionare l'operazione desiderata:");
+		System.out.println("1) versamento");
+		System.out.println("2) prelievo");
+		System.out.println("3) esci");
 	}
 
 }
