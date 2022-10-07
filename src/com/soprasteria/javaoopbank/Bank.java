51bbc5431ce15f9165a27dc26a381381ce1869b0
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-07 15:38:47 +0200

    viene letto il nome del proprietario del conto

--- src/com/soprasteria/javaoopbank/Bank.java
+++ src/com/soprasteria/javaoopbank/Bank.java
@@ -1,10 +1,21 @@
 package com.soprasteria.javaoopbank;
 
+import java.util.Scanner;
+
+import com.soprasteria.javaoopbank.conto.Conto;
+
 public class Bank {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String utente;
 		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("Inserire il proprio nome: ");
+		utente = s.nextLine();
+		
+		Conto myConto = new Conto(utente);
 	}
 
 }
